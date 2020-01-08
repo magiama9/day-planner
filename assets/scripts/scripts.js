07b5cc7e57cbff1d4a3c5b6316a1199e261c7c0b
magiama9 <58517943+magiama9@users.noreply.github.com>
2020-01-08 15:16:50 -0500

    Add commenting and refactor startHour().

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -4,7 +4,8 @@
 // Fetches and formats current time
 var currentTime = moment().format("dddd, MMMM Do YYYY, h:mm A");
 
-var calendarStartHour = 8;
+// Choose a value between 0 - (12 AM) and 23 - (11 PM) for each.
+var calendarStartHour = 9;
 var calendarEndHour = 17;
 
 // Number of events on the calendar
@@ -21,12 +22,12 @@ startHour();
 
 /* TODO - FIND A MORE READABLE WAY TO GENERATE THESE ROWS
 *********************************************************
-  Generate a bootstrap collapsible row                  */
+  Generate a bootstrap collapsible card to display as calendar */
 function makeRow(i) {
   calendarBody.append(
     $("<div>", { class: "card" }).append(
       $("<div>", { class: "card-header", id: "heading" + i }).append(
-        $("<h5>", { class: "mb-0", id:"title"+i }).append(
+        $("<h5>", { class: "mb-0", id: "title" + i }).append(
           $("<button>", {
             class: "btn btn-link",
             "data-toggle": "collapse",
@@ -52,25 +53,30 @@ function makeRow(i) {
   );
 }
 
-// Creates 'num' rows -> num = 9 for a 9-5 workday
+// Creates rows for each hour to display. Calculated as the difference between start and end hour.
 function iterateRows() {
   var numHours = calendarEndHour - calendarStartHour;
   for (let i = 0; i <= numHours; i++) {
     makeRow(i);
     assignTime(i);
   }
-  
 }
 
 // Assigns a time value to each generated row
-function assignTime(num){
-  $("#title"+num).append(
-    $("<span>", {class:"text-right", text:startHour(num, calendarStartHour)})
-  )
+function assignTime(num) {
+  $("#title" + num).append(
+    $("<span>", {
+      class: "text-right",
+      text: startHour(num, calendarStartHour)
+    })
+  );
 }
 
-// 
-function startHour(num, i){
-  startingHour = num + i
-  return moment().hour(startingHour).minute(0).format("h A");
+// Sets the hour to iterate from when labelling rows.
+function startHour(num, i) {
+  startingHour = num + i;
+  return moment()
+    .hour(startingHour)
+    .minute(0)
+    .format("h A");
 }
