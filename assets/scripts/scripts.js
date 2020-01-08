a0b7ffbf3b0c459b73982f8a46dbe97c15ffebd5
magiama9 <58517943+magiama9@users.noreply.github.com>
2020-01-08 14:27:26 -0500

    Implement programmatic generation of first row.

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -8,4 +8,44 @@ var currentTime = moment().format("dddd, MMMM Do YYYY, h:mm A");
 var eventCount = 0;
 var pastEvents = 0;
 var remainingEvents = eventCount - pastEvents;
+
+// Selector for first calendar row
+var firstRow = $("#calendarRow");
 /*****************************************/
+
+makeFirstRow(1);
+
+function generateRow(i) {
+  var newRow = firstRow.clone().attr("id", "calendarRow" + i);
+  newRow.appendTo(firstRow);
+}
+
+function makeFirstRow(i) {
+  $("#calendarRow").append(
+    $("<div>", { class: "card" }).append(
+      $("<div>", { class: "card-header", id: "heading" + i }).append(
+        $("<h5>", { class: "mb-0" }).append(
+          $("<button>", {
+            class: "btn btn-link",
+            "data-toggle": "collapse",
+            "data-target": "#collapse" + i,
+            "aria-expanded": "true",
+            "aria-controls": "collapse" + i
+          }).append($("<span>", { text: "CALENDAR ROW" + i }))
+        )
+      )
+    )
+  );
+  $("#calendarRow").append(
+    $("<div>", {
+      id: "collapse" + i,
+      class: "collapse show",
+      "aria-labelledby": "heading" + i,
+      "data-parent": "#calendarRow"
+    }).append(
+      $("<div>", { class: "card-body" }).append(
+        $("<span>", { text: "EVENT TEXT"+i })
+      )
+    )
+  );
+}
