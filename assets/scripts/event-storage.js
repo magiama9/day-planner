2783cd513e4e725d2fcae4774abb85316200ab68
Sam Randels <samrandels@gmail.com>
2020-01-12 12:43:30 -0500

    Create array of objects. Each object contains information for that hour.

--- assets/scripts/event-storage.js
+++ assets/scripts/event-storage.js
@@ -1,7 +1,42 @@
 let storedEvents = [
     {
         hour: "9 AM",
-        
+        eventList: [],
+    },
+    {
+        hour: "10 AM",
+        eventList: [],
+    },
+    {
+        hour: "11 AM",
+        eventList: [],
+    },
+    {
+        hour: "11 AM",
+        eventList: [],
+    },
+    {
+        hour: "12 PM",
+        eventList: [],
+    },
+    {
+        hour: "1 PM",
+        eventList: [],
+    },
+    {
+        hour: "2 PM",
+        eventList: [],
+    },
+    {
+        hour: "3 PM",
+        eventList: [],
+    },
+    {
+        hour: "4 PM",
+        eventList: [],
+    },
+    {
+        hour: "5 PM",
+        eventList: [],
     }
-    
 ]
