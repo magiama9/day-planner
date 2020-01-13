ccfe82c5e6121e9ae5f32e6bacc9ab6b24413a5c
Sam Randels <samrandels@gmail.com>
2020-01-12 20:58:50 -0500

    Move event storage to main scripts file and update html accordingly.

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,37 +1,94 @@
-$(document).ready(function() {
-/* GLOBAL VARIABLES */
-/*****************************************/
-var currentTime;
 
-// Choose a value between 0 - (12 AM) and 23 - (11 PM) for each.
-var calendarStartHour = 9;
-var calendarEndHour = 17;
+  /* GLOBAL VARIABLES */
+  /*****************************************/
 
-// Number of events on the calendar
-var eventCount = 0;
-var pastEvents = 0;
-var remainingEvents = eventCount - pastEvents;
 
-// Selector for first calendar row
-let calendarBody = $("#calendarBody");
-let eventBody = $(".eventBody");
-let listItem = $("li").hasClass("list-group-item");
 
-// Variable for detecting and coloring list based on whether it has an event
-let hasList;
+  /* LOCAL STORAGE FUNCTION IS SUPER-DUPER BROKEN CURRENTLY 
+     ALSO --- SORRY THE CODE IS SUPER UNREADABLE */
+     
+  var currentTime;
 
-// Global Variables for Storing Form Inputs
-let inputVal = "";
-let inputId = "";
-/*****************************************/
+  // Choose a value between 0 - (12 AM) and 23 - (11 PM) for each.
+  var calendarStartHour = 9;
+  var calendarEndHour = 17;
+
+  // Number of events on the calendar
+  var eventCount = 0;
+  var pastEvents = 0;
+  var remainingEvents = eventCount - pastEvents;
+
+  // Selector for first calendar row
+  let calendarBody = $("#calendarBody");
+  let eventBody = $(".eventBody");
+  let listItem = $("li").hasClass("list-group-item");
+
+  // Variable for detecting and coloring list based on whether it has an event
+  let hasList;
+
+  // Global Variables for Storing Form Inputs
+  let inputVal = "";
+  let inputId = "";
+  let localStorageEvents;
+
+  let storedEvents = [
+    {
+      hour: "09",
+      spanId: "eventBody0",
+      eventList: []
+    },
+    {
+      hour: "10",
+      spanId: "eventBody1",
+      eventList: []
+    },
+    {
+      hour: "11",
+      spanId: "eventBody2",
+      eventList: []
+    },
+    {
+      hour: "14",
+      spanId: "eventBody3",
+      eventList: []
+    },
+    {
+      hour: "13",
+      spanId: "eventBody4",
+      eventList: []
+    },
+    {
+      hour: "14",
+      spanId: "eventBody5",
+      eventList: []
+    },
+    {
+      hour: "15",
+      spanId: "eventBody6",
+      eventList: []
+    },
+    {
+      hour: "16",
+      spanId: "eventBody7",
+      eventList: []
+    },
+    {
+      hour: "17",
+      spanId: "eventBody8",
+      eventList: []
+    }
+  ];
+
+  // Variable used for pushing and fetching from local storage
+
+  /*****************************************/
+  $(document).ready(function() {
 
   pushStoredEvents();
+  updateEventBody();
   iterateRows();
   startHour();
   makeHeader();
-  addButton();
-  isPast(0);
-  displayStoredEvents();
 
   // Creates rows for each hour to display. Calculated as the difference between start and end hour.
   function iterateRows() {
@@ -91,12 +148,6 @@ The time value is prepended to the input-group div and displays as a button/labe
     );
   }
 
-  function addButton() {
-    $(".form-control").append("<i>", {
-      class: "material-icons"
-    });
-    $(".material-icons").text("face");
-  }
   // Assigns a time value to each generated row
   function assignTime(num) {
     $("#title" + num).prepend(
@@ -109,11 +160,8 @@ The time value is prepended to the input-group div and displays as a button/labe
   function isPast(num) {
     let eventHour = $("#title" + num).text();
     let eventHourMoment = moment(eventHour);
-    console.log(eventHourMoment);
     let currentHour = moment().format("HH");
     let currentHourMoment = moment(currentHour);
-    console.log(currentHour);
-    console.log(currentHourMoment.isAfter(eventHourMoment, "HH"));
   }
   // Sets the hour to iterate from when labeling rows.
   function startHour(i, num) {
@@ -144,7 +192,6 @@ The time value is prepended to the input-group div and displays as a button/labe
     let keycode = e.which;
 
     if (keycode === 13) {
-      console.log("this worked");
       inputId = $(this).attr("id");
       inputVal = $(this).val();
 
@@ -167,6 +214,7 @@ The time value is prepended to the input-group div and displays as a button/labe
         $(".eventInput" + i).prepend(
           "<li class='list-group-item border-0'>" + inputVal
         );
+        displayStoredEvents(i);
         toggleCollapse(i);
         colorHeader(i);
         localStorageEvents = JSON.parse(
@@ -199,7 +247,6 @@ The time value is prepended to the input-group div and displays as a button/labe
     $(e.target).remove();
     eventCount--;
     makeHeader();
-    console.log(targetText);
 
     // Determines whether or not the hour has a list item to change the color
 
@@ -207,6 +254,8 @@ The time value is prepended to the input-group div and displays as a button/labe
       let hasList = $("#collapse" + i).has("li");
       if (hasList.length === 0) {
         $("#heading" + i).removeClass("hasEvent");
+      } else if (hasList.length > 0) {
+        $("#heading" + i).addClass("hasEvent");
       }
       // Removes selected value from the stored events array (stored in event-storage.js)
       storedEvents[i].eventList.splice(
@@ -222,9 +271,37 @@ The time value is prepended to the input-group div and displays as a button/labe
       let index = localStorageEvents.indexOf(targetText);
       if (index >= 0) {
         localStorageEvents.splice(targetText, 1);
-        console.log(localStorageEvents);
         localStorage.setItem("storedEvents" + i, localStorageEvents);
       }
     }
   });
+
+
+  // UNFINISHED --- CURRENTLY APPENDING EMPTY EVENTS TO EACH LIST ITEM FROM STORAGE
+  function displayStoredEvents(i) {
+    let eventBodyArr = $("span[id^=eventBody]");
+    let hasList = $("#collapse" + i).has("li");
+    console.log("test");
+      if (
+        storedEvents[i].eventList.length > 0 &&
+        storedEvents[i].spanId == "eventBody" + i
+      ) {
+        for (j = 0; j < storedEvents[i].eventList.length; j++) {
+          $(".eventInput" + i).prepend(
+            "<li class='list-group-item border-0'>" +
+              storedEvents[i].eventList[j]
+          );
+        }
+      }
+    
+  }
+
+  function pushStoredEvents() {
+    for (i = 0; i < storedEvents.length; i++) {
+      localStorageEvents = JSON.parse(
+        localStorage.getItem("storedEvents" + i) || "[]"
+      );
+      storedEvents[i].eventList.push(localStorageEvents);
+    }
+  }
 });
